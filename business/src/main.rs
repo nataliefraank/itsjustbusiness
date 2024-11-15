c8fc00ea3e3ec5d2f67588689acab8bea4177b8c
nataliefraank <nataliefraank@gmail.com>
2024-11-15 14:59:31 -0500

    Updated tiled code

--- business/src/main.rs
+++ business/src/main.rs
@@ -1,21 +1,37 @@
 use bevy::input::keyboard::KeyCode;
 use bevy::prelude::*;
-use bevy::window::PrimaryWindow;
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy_ecs_tiled::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 
 #[derive(Component)]
 struct Person;
 
-fn main() {
-    App::new()
-    .add_plugins(DefaultPlugins)
-    .add_systems(Update, hello_world).run();
-}
-
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     println!("hello world!");
 }
 
+fn main() {
+    App::new()
+        // Add Bevy default plugins
+        .add_plugins(DefaultPlugins)
+        // Add bevy_ecs_tilemap plugin
+        .add_plugins(TilemapPlugin)
+        // Add bevy_ecs_tiled plugin
+        .add_plugins(TiledMapPlugin::default())
+        // Add our startup function to the schedule and run the app
+        .add_systems(Startup, startup)
+        .run();
+}
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawn a 2D camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Load the map: ensure any tile / tileset paths are relative to assets/ folder
+    let map_handle: Handle<TiledMap> = asset_server.load("tilemap_level1.tmx");
+
+    // Spawn the map with default options
+    commands.spawn(TiledMapHandle(map_handle));
+}
