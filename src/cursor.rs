b24df20ae8373d30a2e67969d207417ffb6f5612
willadomm <wdomm@oberlin.edu>
2024-12-10 16:09:29 -0500

    comm

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,26 +1,28 @@
-fn draw_cursor(
-    camera_query: Single<(&Camera, &GlobalTransform)>,
-    windows: Query<&Window>,
-    mut gizmos: Gizmos,
-) {
-    let (camera, camera_transform) = *camera_query;
+// use bevy::prelude::*;
 
-    let Ok(window) = windows.get_single() else {
-        return;
-    };
+// pub fn draw_cursor(
+//     camera_query: Single<(&Camera, &GlobalTransform)>,
+//     windows: Query<&Window>,
+//     mut gizmos: Gizmos,
+// ) {
+//     let (camera, camera_transform) = *camera_query;
 
-    let Some(cursor_position) = window.cursor_position() else {
-        return;
-    };
+//     let Ok(window) = windows.get_single() else {
+//         return;
+//     };
 
-    // Calculate a world position based on the cursor's position.
-    let Ok(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
-        return;
-    };
+//     let Some(cursor_position) = window.cursor_position() else {
+//         return;
+//     };
 
-    gizmos.circle_2d(point, 10., WHITE);
-}
+//     // Calculate a world position based on the cursor's position.
+//     let Ok(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+//         return;
+//     };
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2d);
-}
+//     gizmos.circle_2d(point, 10., WHITE);
+// }
+
+// fn setup(mut commands: Commands) {
+//     commands.spawn(Camera2d);
+// }
