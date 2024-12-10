b0df707d9e1d048f74ae0506a999e1fa8e1ec293
willadomm <wdomm@oberlin.edu>
2024-12-10 15:32:22 -0500

    tasks

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,38 +1,40 @@
-
-#[derive(Resource, Entity)]
-struct Task{ 
-    complete: bool,
-    score: i32,
-}
+// use bevy::prelude::{Entity, Resource};
 
 
-#[derive(Entity)]
-struct PlayerScore {
-    taskscomplete: Vec<Tasks>,
-}
+// #[derive(Resource, Entity)]
+// struct Task{ 
+//     complete: bool,
+//     score: i32,
+// }
+
+
+// #[derive(Entity)]
+// struct PlayerScore {
+//     taskscomplete: Vec<Task>,
+// }
 
 
 
 
-impl Task {
-    fn accomplish(&self){
-        self.complete = true;
-    }
+// impl Task {
+//     fn accomplish(&self){
+//         self.complete = true;
+//     }
 
     
-}
+// }
 
-impl Task for PlayerScore {
-    fn compute_score(&self) -> i32{
-        let counter = 0;
-        for i in &self{
-            counter += i.score;
-        }
+// impl Task for PlayerScore {
+//     fn compute_score(&self) -> i32{
+//         let counter = 0;
+//         for i in &self{
+//             counter += i.score;
+//         }
 
-        return counter
+//         return counter
 
-    }
-}
+//     }
+//}
 
 
 
